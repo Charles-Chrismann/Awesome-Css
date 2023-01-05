86d83bee9c8bddba189cd0ad6dcfd93b7703c196
unknown <charles.chrismann@free.fr>
2023-01-05 22:33:34 +0100

    05-01-2023

---
+++ 05-01-2022/README.md
@@ -0,0 +1,3 @@
+Animating Clip-path using CSS and Vanilla Javascript
+05-01-2022
+https://www.youtube.com/watch?v=3rXD3jmpq9g
---
+++ 05-01-2022/main.js
@@ -0,0 +1,8 @@
+let sec1 = document.querySelector('.sec1');
+let sec2 = document.querySelector('.sec2');
+
+window.addEventListener('scroll', () => {
+  let value = window.scrollY;
+  sec1.style.clipPath = `circle(${value*1.15}px at 0 0)`;
+  sec2.style.clipPath = `circle(${value*1.15}px at 100% 100%)`;
+})

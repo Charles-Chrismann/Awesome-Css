46a0786ee6611e7cd400b47fbdb50901d9fc8512
unknown <charles.chrismann@free.fr>
2023-01-05 22:35:58 +0100

    05-01-2023

--- 05-01-2023/main.js
+++ 05-01-2023/main.js
@@ -4,5 +4,5 @@ let sec2 = document.querySelector('.sec2');
 window.addEventListener('scroll', () => {
   let value = window.scrollY;
   sec1.style.clipPath = `circle(${value*1.15}px at 0 0)`;
-  sec2.style.clipPath = `circle(${value*1.15}px at 100% 100%)`;
+  sec2.style.clipPath = `circle(${value}px at 100% 100%)`;
 })
